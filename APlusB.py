a20ed17fed154431dd0e30e9c47531a19a5e12b7
Zak Bagwell <zakbagwell@outlook.com>
2017-06-19 08:13:11 +0800

    AplusB
    script that adds two numbers

---
+++ APlusB.py
@@ -0,0 +1,20 @@
+# Uses python3
+# There are two ways of running this program:
+# 1. Run
+#     python3 APlusB.py
+# then enter two numbers and press ctrl-d/ctrl-z
+# 2. Save two numbers to a file -- say, dataset.txt.
+# Then run
+#     python3 APlusB.py < dataset.txt
+
+import sys
+import time
+
+input = sys.stdin.read()
+tokens = input.split()
+a = int(tokens[0])
+b = int(tokens[1])
+
+time.sleep(4)
+
+print(a + b)
